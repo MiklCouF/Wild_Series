a67d2c55945f59c0b2daf04ee17c9da6f4427cfd
Michaël Couffignal <michael@couffignal.fr>
2024-06-04 17:33:09 +0200

    quest3

--- server/app/routers/api/programs/router.js
+++ server/app/routers/api/programs/router.js
@@ -3,20 +3,15 @@ const express = require("express");
 const router = express.Router();
 
 /* ************************************************************************* */
-// Import And Use Routers Here
+// Define Your API Routes Here
 /* ************************************************************************* */
 
-const { sayWelcome } = require("../../controllers/sayActions");
+// Import item-related actions
+const { browse } = require("../../../controllers/programActions");
 
-router.get("/", sayWelcome);
+// Route to get a list of items
+router.get("/", browse);
 
-const itemsRouter = require("./items/router");
-
-router.use("/items", itemsRouter);
-
-const programsRouter = require("./programs/router");
-
-router.use("/programs", programsRouter);
 
 /* ************************************************************************* */
 
